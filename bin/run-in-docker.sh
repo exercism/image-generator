7a4b90dfec916d2abde4bca046b306f98a2437a6
Erik Schierboom <erik_schierboom@hotmail.com>
2024-01-30 13:47:22 +0100

    Add basic version

--- bin/run-in-docker.sh
+++ bin/run-in-docker.sh
@@ -18,46 +18,59 @@
 # Stop executing when a command returns a non-zero return code
 set -e
 
-# If any required arguments is missing, print the usage and exit
-if [[ $# -lt 2 ]]; then
-    echo "usage: ./bin/run-in-docker.sh image-url selector [path/to/output/directory/]"
-    exit 1
-fi
+port=9876
+tag="exercism/image-generator"
+platform="linux/amd64"
 
-image_url="${1}"
-selector="${2}"
-container_port=9876
-
-# Build the Docker image, unless SKIP_BUILD is set
 if [[ -z "${SKIP_BUILD}" ]]; then
-    docker build --rm -t exercism/image-generator .
+    docker build --platform "${platform}" -t "${tag}" .
 fi
 
-# Run the Docker image using the settings mimicking the production environment
-# The shm-size is set to 2gb as otherwise the page crashes when visiting
-container_id=$(docker run \
-    --shm-size=2gb \
-    --detach \
-    --publish ${container_port}:8080 \
-    exercism/image-generator)
+container_id=$(docker run -d --platform "${platform}" -p ${port}:8080 "${tag}")
 
-echo "generating image..."
-
-#  the function with the correct JSON event payload
-body_json=$(jq -n --arg url "${image_url}" --arg selector "${selector}" '{url: $url, selector: $selector}')
-event_json=$(jq -n --arg b "${body_json}" '{body: $b}')
-echo $event_json
-function_url="http://localhost:${container_port}/2015-03-31/functions/function/invocations"
-
-if [ -z "${3}" ]; then
-    curl -XPOST "${function_url}" --data "${event_json}"
-else
-    output_dir=$(realpath "${3%/}")
-    mkdir -p "${output_dir}"
-    curl -XPOST "${function_url}" --data "${event_json}" --silent > "${output_dir}/response.json"
-    jq -r '.body' "${output_dir}/response.json" | base64 --decode > "${output_dir}/image.png"
-fi
-
-echo "done"
+curl "http://localhost:${port}/2015-03-31/functions/function/invocations" -d '{"payload": {"name": "Spielberg"}}'
+echo ''
 
 docker stop $container_id > /dev/null
+
+# # If any required arguments is missing, print the usage and exit
+# # if [[ $# -lt 2 ]]; then
+# #     echo "usage: ./bin/run-in-docker.sh image-url selector [path/to/output/directory/]"
+# #     exit 1
+# # fi
+
+# image_url="${1}"
+# selector="${2}"
+# port=9876
+# tag="exercism/image-generator"
+# platform="linux/amd64"
+
+
+
+# # Run the Docker image using the settings mimicking the production environment
+# container_id=$(docker run --platform "${platform}" -p ${port}:8080 "${tag}")
+
+# echo "generating image..."
+
+# curl "http://localhost:${port}/2015-03-31/functions/function/invocations" -d '{}'
+
+# docker stop $container_id
+
+# # #  the function with the correct JSON event payload
+# # body_json=$(jq -n --arg url "${image_url}" --arg selector "${selector}" '{url: $url, selector: $selector}')
+# # event_json=$(jq -n --arg b "${body_json}" '{body: $b}')
+# # echo $event_json
+# # function_url="http://localhost:${port}/2015-03-31/functions/function/invocations"
+
+# # if [ -z "${3}" ]; then
+# #     curl -XPOST "${function_url}" --data "${event_json}"
+# # else
+# #     output_dir=$(realpath "${3%/}")
+# #     mkdir -p "${output_dir}"
+# #     curl -XPOST "${function_url}" --data "${event_json}" --silent > "${output_dir}/response.json"
+# #     jq -r '.body' "${output_dir}/response.json" | base64 --decode > "${output_dir}/image.png"
+# # fi
+
+# # echo "done"
+
+# # docker stop $container_id > /dev/null
--- bin/run.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-require("./lib/image_generator")
-
-event = JSON.parse(ARGV[0])
-response = ImageGenerator.process(event: event, context: {})
-puts response.to_json
